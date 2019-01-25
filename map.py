1dab245cdc672bfdbcaa1e631020bc51732f44ef
Alex Hughes <hughes.13.alex@gmail.com>
2019-01-24 21:34:44 -0800

    First Commit

---
+++ map.py
@@ -0,0 +1,52 @@
+# pip install folium
+
+import folium
+
+map = folium.Map(location=[80, -100])  # create map class object, location must be in lat and long
+# must translate object to html
+map.save("Map1.html")  # using the save method, pass the name for the map file that will be created
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
