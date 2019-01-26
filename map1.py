90b2f0f58251385696c178ff76032bf7dc4d4557
Alex Hughes <hughes.13.alex@gmail.com>
2019-01-25 16:00:03 -0800

    Third Commit

--- map1.py
+++ map1.py
@@ -1,7 +1,7 @@
 # pip install folium
 
 import folium
-map = folium.Map(location=[36.778259, -119.417931], zoom_start=6)  # set map's starting location lon/lat
+map = folium.Map(location=[36.778259, -119.417931], zoom_start=6, tiles="Mapbox Bright")  # set map's starting location lon/lat
 # set starting zoom level for this location
 map.save("Map1.html")  # using the save method, pass the name for the map file that will be created
 
